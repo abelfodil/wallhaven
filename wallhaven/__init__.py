b1c04022c5e6de72ed0223dd84143f9c819586db
Lucas Cavalcante da Silva <silva.lucasdev@gmail.com>
2020-01-26 19:23:54 -0300

    Fix imports

--- wallhaven/__init__.py
+++ wallhaven/__init__.py
@@ -3,5 +3,5 @@ __version__ = "0.5.0"
 # Use of this source code is governed by the MIT license.
 __license__ = "MIT"
 
-from main import Wallhaven
-from params import Parameters
+from .main import Wallhaven
+from .params import Parameters
