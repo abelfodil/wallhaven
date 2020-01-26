7b69ea2a5d1eef7b605f0432d76920c72c6281a3
Lucas Cavalcante da Silva <silva.lucasdev@gmail.com>
2020-01-26 19:26:02 -0300

    Bump version to 0.5.3

--- wallhaven/__init__.py
+++ wallhaven/__init__.py
@@ -1,5 +1,5 @@
 __author__ = "Lucas Cavalcante da Silva <silva.lucasdev@gmail.com>"
-__version__ = "0.5.0"
+__version__ = "0.5.3"
 # Use of this source code is governed by the MIT license.
 __license__ = "MIT"
 
