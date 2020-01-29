907b69f0ddf1296e6028bc78f1b76701003907fe
Lucas Cavalcante da Silva <silva.lucasdev@gmail.com>
2020-01-29 14:52:52 -0300

    Update files.
    Improved general code quality.
    Added comments.
    Now raises an exception when an argument is not of the expected type.
    Added some filters to the search query.
    Fixed type-hints in some methods and arguments.
    Changed methods' documentation style.
    Changed some error messages.

--- wallhaven/main.py
+++ wallhaven/main.py
@@ -1,20 +1,20 @@
-import json
 from typing import Union, Dict, List
 
 import requests
 
-from wallhaven.exceptions import *
+from wallhaven.exceptions import ApiKeyError, PageNotFoundError
+from wallhaven.params import Parameters
 
 
 class Wallhaven:
     """
-    A Python wrapper around the Wallhaven API.
-    
-    Sample usage: 
-    from wallhaven import Wallhaven
-    
-    wallhaven = Wallhaven()
-    data = wallhaven.get_wallpaper_info(wallpaper_id)
+        A Python wrapper around the Wallhaven API.
+
+        Sample usage:
+        from wallhaven import Wallhaven
+
+        wallhaven = Wallhaven()
+        data = wallhaven.get_wallpaper_info(wallpaper_id)
     """
 
     BASE_URL = "https://wallhaven.cc/api/v1/"
@@ -28,103 +28,232 @@ class Wallhaven:
         self.api_key = api_key
 
     @staticmethod
-    def _request(url, **kwargs) -> requests.Response:
-        return requests.get(url, **kwargs)
+    def _request(url: str, **kwargs) -> requests.Response:
+        """
+            Perform a request at url.
+
+            :param url: Url.
+            :param **kwargs: Additional keyword arguments `requests` take.
+        """
+
+        # Use session for multiple requests to the same server.
+        # It will speed up your requests.
+        # Using a context manager will automatically close the session.
+        with requests.Session() as session:
+            return session.get(url, **kwargs)
 
     def get_wallpaper_info(self, wallpaper_id: Union[str, int]) -> Dict:
-        """ Return wallpaper metadata in JSON format. 
-        @param wallpaper_id: Wallpaper ID in a string format. Example: 'r25peq'
+        """
+            Return wallpaper metadata from `wallpaper_id`
+
+            :param wallpaper_id: Wallpaper ID in a string format.
         """
         url = self.WALLPAPER_URL + str(wallpaper_id)
 
-        params = {"apikey": self.api_key}
-        response = self._request(url, timeout=10, params=params)
+        # Request url.
+        response = self._request(url, timeout=10)
 
+        # Unauthorized. API key is wrong or inexistent.
+        # If user is unauthorized, is either because he tried to access
+        # something without an API key, or the API key is not valid.
         if response.status_code == 401:
             if self.api_key is None:
-                raise ApiKeyError("You need an API key to access NSFW wallpapers.")
-            else:
-                raise ApiKeyError("Invalid API key.")
+                raise ApiKeyError(
+                    "ERROR! -> You need an API key to access NSFW wallpapers."
+                )
+            raise ApiKeyError("ERROR! -> Invalid API key.")
 
-        if not response.ok:
-            raise RequestError("Error requesting server: " + url)
-
+        # Return data
         return response.json()["data"]
 
-    def get_tag_info(self, tag_id: Union[str, int]) -> Dict:
-        """ Return tag metadata in JSON format. 
-        @param tag_id: Tag id. 
+    def get_tag_info(self, tag_id: str) -> Dict:
+        """
+            Return tag metadata from `tag_id`.
+
+            :param tag_id: Tag id.
         """
 
+        # Only allow strings
+        if not isinstance(tag_id, str):
+            raise TypeError(f"ERROR! -> Expected type `str`. Found: {type(tag_id)}")
+
+        # Format URL and make a request.
         url = self.TAG_URL + str(tag_id)
         response = self._request(url, timeout=10)
+
+        # Tag ID doesn't exist.
         if response.status_code == 404:
-            raise PageNotFoundError("Tag does not exist.")
+            raise PageNotFoundError("ERROR! -> Tag does not exist.")
 
-        if not response.ok:
-            raise RequestError("Error requesting server: " + url)
-
+        # Return data
         return response.json()["data"]
 
     def get_user_settings(self) -> Dict:
-        """ Return user settings in a JSON format. An API key must be provided."""
-        if self.api_key is None:
-            raise ApiKeyError("Missing API key")
-
-        params = {"apikey": self.api_key}
-        response = self._request(self.SETTINGS_URL, params=params, timeout=10)
-        if response.status_code == 404:
-            raise RequestError("Invalid API key.")
-        if not response.ok:
-            raise RequestError("Error requesting user settings.")
-
-        return response.json()["data"]
-
-    def get_collection_from_username(self, username: str) -> Dict:
-        """ Return user's public collection in JSON format. 
-        @param username: Collection's owner.
+        """
+            Return user settings. A valid API key must be provided.
         """
 
+        # Check if API key exists.
+        if self.api_key is None:
+            raise ApiKeyError("ERROR! -> Missing API key")
+
+        # Add key to `params`.
+        params = {"apikey": self.api_key}
+
+        # Request url.
+        response = self._request(self.SETTINGS_URL, params=params, timeout=10)
+
+        # Page is not found if API key is INVALID.
+        if response.status_code == 404:
+            raise ApiKeyError("ERROR! -> API key is not valid!")
+
+        # Return data if no errors occurred.
+        return response.json()["data"]
+
+    def get_collections_from_username(self, username: str) -> List:
+        """
+            Return user's public collection from `username`.
+
+            :param username: Collection's owner.
+        """
+
+        # Format URL and make request.
         url = self.COLLECTIONS_URL + str(username)
         response = self._request(url, timeout=10)
-        if response.status_code == 404:
-            raise PageNotFoundError("User doesn't exist.")
-        if not response.ok:
-            raise RequestError("Error requesting server: " + url)
 
+        # Invalid username.
+        if response.status_code == 404:
+            raise PageNotFoundError("ERROR! -> Invalid username.")
+
+        # Return data if no errors occurred.
         return response.json()["data"]
 
-    def get_collection_from_apikey(self) -> Dict:
-        """ Return user collection in JSON format. 
-        An API key must be provided for this to work.
+    def get_collections_from_apikey(self) -> List:
+        """
+            Return user collection. A valid API key must be provided.
         """
 
+        # Check if API key exists.
         if self.api_key is None:
-            raise ApiKeyError("Missing API key.")
+            raise ApiKeyError("ERROR! -> Missing API key.")
 
+        # Add key to `params`.
         params = {"apikey": self.api_key}
         response = self._request(self.COLLECTIONS_URL, params=params, timeout=10)
-        if response.status_code == 401:
-            raise ApiKeyError("Invalid API key.")
-        if not response.ok:
-            raise RequestError("Error requesting user's collection.")
 
+        # Unauthorized if API key is not valid.
+        if response.status_code == 401:
+            raise ApiKeyError("ERROR! -> Invalid API key.")
+
+        # Return data if no errors occurred.
         return response.json()["data"]
 
-    def search(
-        self, params: Union[Dict[str, str], "wallhaven.search.Parameters"]
-    ) -> Dict:
-        """ Search wallpapers using the user's defined parameters. """
-        if not isinstance(params, dict):
-            params = params.get_params()
+    def get_wallpapers_from_user_collection(
+        self, username: str, collection_id: Union[int, str], limit: int = 0
+    ) -> List:
+        """
+            Return wallpapers from a user's collection.
 
+            :param username: Collections' Owner.
+            :param collection_id: Collection id.
+            :param limit. Limit the amount of wallpapers returned.
+        """
+
+        if not isinstance(username, str):
+            raise TypeError(
+                f"ERROR! -> Invalid username. Expected type `str`. Found: {type(username)}"
+            )
+
+        if not isinstance(collection_id, int) and not isinstance(collection_id, str):
+            raise TypeError(
+                f"ERROR! -> Invalid collection id. Expected type `str` or `int`. Found: {type(collection_id)}"
+            )
+
+        if not isinstance(limit, int):
+            raise TypeError(
+                f"ERROR! -> Invalid limit. Expected type `int`. Found: {type(limit)}"
+            )
+
+        # Format URL. Default page is 1.
+        url = self.COLLECTIONS_URL + username + "/" + str(collection_id)
+        response = self._request(url, timeout=10)
+
+        # User not found;
+        if response.status_code == 404:
+            raise PageNotFoundError("ERROR! -> User not found.")
+
+        # Get list of wallpapers.
+        data = response.json()["data"]
+
+        # Data is empty if collection doesn't exist.
+        if not data:
+            return data
+
+        # Return the amount of wallpapers defined by limit
+        # if limit is greater than, or equal to, data.
+        if limit and len(data) >= limit:
+            return data[0:limit]
+
+        # Get amount of pages and wallpapers.
+        meta = response.json()["meta"]
+
+        # Start from page 2
+        page = 2
+
+        # Another list to store all wallpapers.
+        wallpapers = data.copy()
+        while True:
+            # Meta has the total amount of pages needed to request all walls.
+            # There are 24 wallpapers per page.
+            # If page is 3 and `meta["last_page"]` is 2, there are no more pages.
+            if page > meta["last_page"]:
+                break
+
+            # Request images and get data.
+            # We know `data` is never going to be empty
+            # because of the page check.
+            response = self._request(url, timeout=10, params={"page": page})
+            data = response.json()["data"]
+
+            # Loops through data.
+            for wall in data:
+                if not limit:
+                    wallpapers.append(wall)
+                elif len(wallpapers) >= limit:
+                    break
+
+            # Increment page.
+            page += 1
+
+        # Return amount of wallpapers.
+        return wallpapers
+
+    def search(self, parameters: Union[Dict[str, str], Parameters]) -> Dict:
+        """
+            Search wallpapers using the defined parameters.
+
+            :param parameters: Parameters to filter wallpapers.
+        """
+
+        # Search only works with a dictionary of parameters.
+
+        # Check if `parameters` is not a dict.
+        if not isinstance(parameters, dict) and isinstance(parameters, Parameters):
+            # Get a dictionary with parameters.
+            # Also possible to use `parameters.params`.
+            params = parameters.get_params()
+        elif not isinstance(parameters, Parameters):
+            raise TypeError(
+                f"ERROR! -> Invalid parameters. Expected type `Dict` or `Parameters`. Found: {type(parameters)}"
+            )
+
+        # Check if API key exists.
         if self.api_key is not None:
             params["apikey"] = self.api_key
 
+        # Make request.
         response = self._request(self.SEARCH_URL, params=params, timeout=10)
-        if not response.ok:
-            raise RequestError("Error: " + str(response.status_code))
 
-        # Return an empty list if API key is invalid (or missing) and NSFW is set to True.
+        # Return an empty list if API key is invalid (or missing)
+        # and NSFW is set to True.
         return response.json()["data"]
-
--- wallhaven/params.py
+++ wallhaven/params.py
@@ -1,30 +1,29 @@
-from typing import Union, Dict
+from typing import Union, Dict, Iterable
 
-from wallhaven.exceptions import *
+from wallhaven.exceptions import OptionError
 
 
 class Parameters:
     """
     Usage:
-
     from wallhaven import Wallhaven, Parameters
-    
     walhaven = Wallhaven()
-    
+
     params = Parameters()
     params.set_categories(general=True, anime=False, people=False)
     params.set_sorting(sorting="toplist")
     params.set_range("Last Three Days")
-    
     data = wallhaven.search(params=params)
     """
 
     def __init__(self):
         # Reset parameters.
-        self.defaultParameters()
+        self.reset_params()
 
-    def defaultParameters(self):
-        """ Reset parameters. """
+    def reset_params(self):
+        """
+            Reset parameters.
+        """
         self.params = {
             "categories": "111",
             "purity": "100",
@@ -35,7 +34,7 @@ class Parameters:
             "q": "",
         }
 
-    def get_params(self) -> Dict[str, str]:
+    def get_params(self) -> Union[Dict[str, str], Dict]:
         """ Return the current parameters. """
         return self.params
 
@@ -45,13 +44,37 @@ class Parameters:
         anime: Union[bool, str, int] = True,
         people: Union[bool, str, int] = True,
     ) -> None:
-        """ Turn categories on(True) or off(False). At least 1 (one) category is needed."""
+        """
+            Turn categories on (True, "1", 1) or off (False, "0", 0).
+            At least 1 (one) category is needed.
+
+            :param general: Includes general images. Neither anime nor people.
+            :param anime: Includes anime related images.
+            :param people: Includes images of people.
+        """
+
+        # Available categories.
         categories = [general, anime, people]
 
-        if not any(categories):
-            raise InvalidOptionError("At least one (1) category must be included.")
+        # raise TypeError if category is not a boolean, a string, or an integer.
+        for category in categories:
+            if (
+                not isinstance(category, bool)
+                and not isinstance(category, str)
+                and not isinstance(category, int)
+            ):
+                raise TypeError(
+                    f"ERROR! -> Expected `bool`, `str` or `int`. Found: {type(category)}"
+                )
 
+        # Check if the user chose at least one category.
+        if not any(categories):
+            raise OptionError("At least one (1) category must be included.")
+
+        # Convert category to "1" or "0".
         category_string = "".join([str(int(category)) for category in categories])
+
+        # Set category.
         self.params["categories"] = category_string
 
     def set_purity(
@@ -60,18 +83,60 @@ class Parameters:
         sketchy: Union[bool, str, int] = False,
         nsfw: Union[bool, str, int] = False,
     ) -> None:
-        """ Turn purities on(True) or off(False). At least 1 (one) purity is needed. 
-        NSFW requires a valid API key."""
+        """
+            Turn purities on (True, "1", 1) or off (False, "0", 0).
+            At least 1 (one) purity is needed. NSFW requires a valid API key.
+
+            :param sfw: Includes safe-for-work images.
+            :param sketchy: Includes sketchy (not quite sfw not quite nsfw) images.
+            :param nsfw: Includes not-safe-for-work (mature) images.
+        """
+
+        # Available purity.
         purity_list = [sfw, sketchy, nsfw]
 
-        if not any(purity_list):
-            raise InvalidOptionError("At least one (1) purity must be included.")
+        # raise TypeError if purity is not a boolean, a string, or an integer.
+        for purity in purity_list:
+            if (
+                not isinstance(purity, bool)
+                and not isinstance(purity, str)
+                and not isinstance(purity, int)
+            ):
+                raise TypeError(
+                    f"ERROR! -> Expected `bool`, `str` or `int`. Found: {type(purity)}"
+                )
 
+        # Check if the user chose at least one purity.
+        if not any(purity_list):
+            raise OptionError("ERROR! -> At least one (1) purity must be included.")
+
+        # Convert purity to "1" and "0".
         purity_string = "".join([str(int(purity)) for purity in purity_list])
+
+        # Set purity.
         self.params["purity"] = purity_string
 
     def set_sorting(self, sorting: str = "Date Added") -> None:
-        """ Set the method of sorting results. """
+        """
+            Set the method of sorting results.
+
+            :param sorting: Sorting method.
+
+            Available methods:
+            - Date Added
+            - Relevance (related to search query)
+            - Random
+            - Views
+            - Favorites
+            - Toplist
+            - Toplist Beta
+        """
+
+        # Check if sorting is not a string.
+        if not isinstance(sorting, str):
+            raise TypeError(f"ERROR! -> Expected `str`. Found: {type(sorting)}")
+
+        # List of available sortings.
         available_sortings = [
             "date_added",
             "relevance",
@@ -81,14 +146,36 @@ class Parameters:
             "toplist",
             "toplist_beta",
         ]
+
+        # Transforms `sorting`.
+        # Date Added -> date_added
         sorting = sorting.lower().replace(" ", "_")
+
+        # Check for exact matches.
         if sorting not in available_sortings:
-            raise InvalidOptionError("Invalid sorting: " + sorting)
+            raise OptionError("ERROR! -> Invalid sorting: " + sorting)
 
         self.params["sorting"] = sorting
 
     def set_range(self, top_range: str = "Last Month") -> None:
-        """ Set the time range. Sorting MUST be toplist for this to work. """
+        """
+            Set the time range. Sorting MUST be toplist for this to work.
+
+            :param top_range: Range.
+
+            Available ranges:
+            - Last Day (or 1d)
+            - Last Three Days (or 3d)
+            - Last Week (or 1w)
+            - Last Month (or 1M)
+            - Last Three Months (3M)
+            - Last 6 Months (6M)
+            - Last Year (1y)
+        """
+
+        if not isinstance(top_range, str):
+            raise TypeError(f"ERROR! -> Expected `str`. Found: {type(top_range)}")
+
         range_mapping = {
             "last_day": "1d",
             "last_three_days": "3d",
@@ -99,48 +186,180 @@ class Parameters:
             "last_year": "1y",
         }
 
+        # Transforms `top_range` in a string that can match the mapping keys.
+        # Last Month -> last_month
         top_range = top_range.lower().replace(" ", "_")
+
+        # Check if `top_range` is either the key or the value.
+        # Allows `top_range` to be "Last Month" or "1M".
         for key, value in range_mapping.items():
-            if top_range in key:
+            if top_range == key:
                 top_range = value
                 break
-            elif top_range in value:
+            if top_range == value:
                 break
-            else:
-                continue
         else:
-            raise InvalidOptionError("Invalid range.")
+            raise OptionError(f"ERROR! -> Invalid range: {top_range}")
 
         self.params["topRange"] = top_range
 
     def set_sorting_order(self, order: str = "Descending") -> None:
-        """ Set the sorting order. """
+        """
+            Set the sorting order.
+
+            :param order: Sorting order. Can be either Descending or Ascending.
+        """
+
+        # Check if order is not a string.
+        if not isinstance(order, str):
+            raise TypeError(f"Expected `str`. Found: {type(order)}")
+
+        # Map all possible orders.
         order_mapping = {"descending": "desc", "ascending": "asc"}
+
+        # Transforms `order` in a string that can match the mapping keys or values.
         order = order.lower().replace(" ", "_")
 
+        # Check if `order` is in the mapping.
         for key, value in order_mapping.items():
-            if order in key:
+            if order == key:
                 order = value
                 break
-            elif order in value:
+            if order == value:
                 break
-            else:
-                continue
-        else:
-            raise InvalidOptionError("Invalid order.")
 
+        # Runs if `order` is not in the mapping.
+        else:
+            raise OptionError(f"ERROR! -> Invalid order: {order}")
+
+        # Set the order if nothing went wrong.
         self.params["order"] = order
 
     def set_page(self, page_number: Union[str, int] = "1") -> None:
-        """ Defines what page number to request. """
+        """
+            Defines what page number to request.
+
+            :param page_number: Page.
+        """
+
+        # Check `page_number` type.
+        # raises TypeError if `page_number` is not a string nor a integer.
+        if not isinstance(page_number, str) and not isinstance(page_number, int):
+            raise TypeError(
+                f"ERROR! -> Expected type `str` or `int`. Found: {type(page_number)}"
+            )
+
+        # Convert to string in case `page_number` is an integer.
         page = str(page_number)
         if not page.isnumeric():
-            raise InvalidOptionError("Invalid page. Must be a number!")
+            raise TypeError("ERROR! -> Page needs to be a numeric value.")
 
+        # Set page.
         self.params["page"] = page_number
 
-    def set_search_query(self, query: str = ""):
-        """ Set the search query in the parameters."""
-        query = str(query)
+    def set_search_query(self, query: str = "") -> None:
+        """
+            Set the search query in the parameters.
+            Overwrites the last search query (including tags and others filters).
+
+            :param query: Search query.
+
+            Current filters:
+            tagname (normal query) -> Search for a tag/keyword.
+            -tagname -> Exclude a tag/keyword.
+            +tag1 +tag2 -> Must have tag1 and tag2.
+            +tag1 -tag2 -> Must have tag1 and NOT tag2.
+            @username -> User uploads
+            id:123 -> Exact tag search (cannot be used with +tag and/or -tag)
+            type:{png/jpg} -> Search for file type (jpg = jpeg)
+            like:<wallpaper-id> -> Find wallpaper with similar tags.
+        """
+
+        if not isinstance(query, str):
+            raise TypeError(f"Expected `str`. Found: {type(query)}")
 
         self.params["q"] = query
+
+    def include_tags(self, tags: Iterable[str]) -> None:
+        """
+            Include tags. Only show images with matching tags.
+
+            :param tags: An iterable of tags.
+        """
+
+        # Check if `tags` is not an iterable (excluding strings).
+        if not isinstance(tags, Iterable) or isinstance(tags, str):
+            raise TypeError(f"ERROR! -> Expected `Iterable[str]`. Found: {type(tags)}")
+
+        # Check if `tags` if empty.
+        if not tags:
+            raise OptionError(f"Can't include tags from: {tags}")
+
+        for tag in tags:
+            # End with white space to allow more filters.
+            tag = f"+{tag} ".lower()
+
+            # Don't allow duplicates.
+            if tag not in self.params["q"]:
+
+                # Check if the end of the search query is a white space or not.
+                # This avoids tags sticking together.
+                # Example: Query: +anime+music
+                # This only happens if the user added the tags from `set_search_query`
+                if not self.params["q"][0] == "":
+                    self.params["q"] += f" {tag}"
+                else:
+                    self.params["q"] += tag
+
+    def exclude_tags(self, tags: Iterable[str]) -> None:
+        """
+            Exclude tags. Only show images with no matching tags.
+
+            :param tags: An iterable of tags.
+        """
+
+        # Check if `tags` is not an iterable (excluding strings).
+        if not isinstance(tags, Iterable) or isinstance(tags, str):
+            raise TypeError(f"ERROR! -> Expected `Iterable[str]`. Found: {type(tags)}")
+
+        # Check if `tags` is empty.
+        if not tags:
+            raise OptionError(f"Can't exclude tags from: {tags}")
+
+        # Add tag to search query.
+        for tag in tags:
+            # End with white space to allow more filters.
+            tag = f"-{tag} ".lower()
+
+            # Don't allow duplicates
+            if tag not in self.params["q"]:
+
+                # Check if the end of the search query is a white space or not.
+                # This avoids tags sticking together.
+                # Example: Query: +anime+music
+                # This only happens if the user added the tags from `set_search_query`
+                if not self.params["q"][0] == "":
+                    self.params["q"] += f" {tag}"
+                else:
+                    self.params["q"] += tag
+                self.params["q"] += tag
+
+    def filter_wallpapers_by_user(self, username: str) -> None:
+        """
+            Only returns images by `username`.
+
+            :param username: Username
+        """
+
+        # Check if `username` is not a string.
+        if not isinstance(username, str):
+            raise TypeError(
+                f"ERROR! -> Invalid username. Expected `str`. Found: {type(username)}"
+            )
+
+        # Check if `username` is only white spaces.
+        if not username:
+            raise OptionError("ERROR! -> Username cannot be blank.")
+
+        # End with space to allow more filters.
+        self.params["q"] += f"@{username} "
