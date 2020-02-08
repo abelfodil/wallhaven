b79c8ab0d619dbb2704d22fbb41e43e6ec33fa07
Lucas Cavalcante da Silva <silva.lucasdev@gmail.com>
2020-02-08 19:05:42 -0300

    Add exception

--- wallhaven/exceptions.py
+++ wallhaven/exceptions.py
@@ -1,14 +1,17 @@
 class ApiKeyError(Exception):
     """
-        An error that occurs when the API key is missing or not valid.
+    An error that occurs when the API key is missing or not valid.
     """
-
-    pass
 
 
 class PageNotFoundError(Exception):
     """
-        An error that occurs when the requested page is not found.
+    An error that occurs when the requested page is not found.
     """
 
-    pass
+
+class RequestLimitError(Exception):
+    """
+    An error that occurs when the user reaches the request limit.
+    Limit is 45 requests per minute.
+    """
