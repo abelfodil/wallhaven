7d818f66817099d9cfbbc2a1234ad2b856f6364d
Lucas Cavalcante da Silva <silva.lucasdev@gmail.com>
2020-07-01 17:06:06 -0300

    Fix import and remove author and license.

--- wallhaven/__init__.py
+++ wallhaven/__init__.py
@@ -1,7 +1,2 @@
-__author__ = "Lucas Cavalcante da Silva <silva.lucasdev@gmail.com>"
-__version__ = "0.6.6"
-# Use of this source code is governed by the MIT license.
-__license__ = "MIT"
-
-from .main import Wallhaven
-from .params import Parameters
+from .api import Wallhaven
+from .config import Parameters
