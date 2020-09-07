56e45a492cdfbdd58aecd229bf0bb00d6ef01e27
Lucas Cavalcante da Silva <silva.lucasdev@gmail.com>
2020-09-07 18:30:26 -0300

    refactor: improve models, remove attrs and update dependencies.

--- wallhaven/models.py
+++ wallhaven/models.py
@@ -1,171 +1,246 @@
-from typing import List, Dict
+from typing import Dict, Any
+import json
 
-import attr
-
-
-@attr.s(frozen=True)
-class Tag:
-    id: int = attr.ib()
-    name: str = attr.ib()
-    alias: str = attr.ib()
-    category_id: int = attr.ib()
-    category: str = attr.ib()
-    purity: str = attr.ib()
-    created_at: str = attr.ib()
-
-    @classmethod
-    def from_data(cls, data) -> "Tag":
-        """
-        Return an instance of "Tag" from a given data.
-
-        :param data: Tag information in JSON format.
-        """
-        return cls(**data)
+from wallhaven.utils import dict_to_string
 
 
-@attr.s(frozen=True)
-class Uploader:
-    username: str = attr.ib()
-    group: str = attr.ib()
-    avatar: str = attr.ib()
+class WallhavenModel:
+    """Base class from which all models will inherit."""
 
-    @classmethod
-    def from_data(cls, data) -> "Uploader":
-        """
-        Return an instance of `Uploader` from a given data.
+    def __init__(self, **kwargs):
+        self._default_params = {}
 
-        :param data: Uploader data in JSON format.
-        """
-        return cls(**data)
+        # This will be populated when `new_from_dict` is called.
+        self._json = None
 
-
-@attr.s(frozen=True)
-class Wallpaper:
-    id: str = attr.ib()
-    url: str = attr.ib()
-    short_url: str = attr.ib()
-    uploader: Uploader = attr.ib()
-    views: int = attr.ib()
-    favorites: int = attr.ib()
-    source: str = attr.ib()
-    purity: str = attr.ib()
-    category: str = attr.ib()
-    dimension_x: int = attr.ib()
-    dimension_y: int = attr.ib()
-    resolution: str = attr.ib()
-    ratio: str = attr.ib()
-    file_size: int = attr.ib()
-    file_type: str = attr.ib()
-    created_at: str = attr.ib()
-    colors: List[str] = attr.ib()
-    path: str = attr.ib()
-    thumbs: Dict[str, str] = attr.ib()
-    tags: List[Tag] = attr.ib()
-
-    @classmethod
-    def from_data(cls, data) -> "Wallpaper":
-        """
-        Return an instance of `Wallpaper` from a given data.
-
-        :param data: Wallpaper information in JSON format.
-        """
-
-        # Get tags
-        tags = [Tag.from_data(tag) for tag in data.pop("tags")]
-
-        # Get uploader
-        uploader = Uploader.from_data(data.pop("uploader"))
-
-        # Add them back to data.
-        data["tags"] = tags
-        data["uploader"] = uploader
-
-        # Return an instance of wallpaper
-        return cls(**data)
-
-    @classmethod
-    def from_collection_data(cls, data) -> "Wallpaper":
-        """
-        Return an instance of `Wallpaper` from a given collection data.
-        The wallpaper information returned by the collection has neither
-        tags nor an uploader.
-
-        :param data: Wallpaper information in JSON format.
-        """
-
-        # Add tags and uploader to data.
-        data["tags"] = None
-        data["uploader"] = None
-
-        return cls(**data)
-
-
-@attr.s(frozen=True)
-class Settings:
-    thumb_size: str = attr.ib()
-    per_page: str = attr.ib()
-    purity: List[str] = attr.ib()
-    categories: List[str] = attr.ib()
-    resolutions: List[str] = attr.ib()
-    aspect_ratios: List[str] = attr.ib()
-    toplist_range: str = attr.ib()
-    tag_blacklist: List[str] = attr.ib()
-    user_blacklist: List[str] = attr.ib()
-
-    @classmethod
-    def from_data(cls, data) -> "Settings":
-        """
-        Return an instance of `Settings` from a given data.
-
-        :param data: Settings information in JSON format.
-        """
-        return cls(**data)
-
-
-@attr.s(frozen=True)
-class Collection:
-    id: int = attr.ib()
-    label: str = attr.ib()
-    views: int = attr.ib()
-    public: int = attr.ib()
-    count: int = attr.ib()
-
-    @classmethod
-    def from_data(cls, data) -> "Collection":
-        """
-        Return an instance of `Collection` from a given data.
-
-        :param data: Collection information in JSON format.
-        """
-        return cls(**data)
-
-
-@attr.s(frozen=True)
-class CollectionData:
-    data: List[Wallpaper] = attr.ib()
-    current_page: int = attr.ib()
-    last_page: int = attr.ib()
-    per_page: int = attr.ib()
-    total: int = attr.ib()
-    url: str = attr.ib()
-
-    @classmethod
-    def from_data(cls, data) -> "CollectionData":
-        """
-        Return an instance of `CollectionData` from a given data.
-
-        :param data: Collection data (wallpapers and pagination) in JSON format.
-        """
-        # Get wallpapers removing 'data' from the dictionary.
-        walls = [Wallpaper.from_collection_data(wall) for wall in data.pop("data")]
-        meta = data["meta"]
-        # I think this is a good way to do this, but mypy disagrees.
-        # It still works but this may change this in the future.
-        return cls(
-            data=walls,
-            current_page=meta["current_page"],
-            last_page=meta["last_page"],
-            per_page=meta["per_page"],
-            total=meta["total"],
-            url=meta["url"],
+    def __repr__(self):
+        """Return a string that can be converted into an instance with `eval`."""
+        return "{class_name}({attributes})".format(
+            class_name=self.__class__.__name__,
+            attributes=dict_to_string(self.as_dict()),
         )
+
+    def __eq__(self, other):
+        return self.as_dict() == other.as_dict()
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def as_dict(self) -> Dict[str, Any]:
+        """Return the instance as a dictionary."""
+        data: Dict[str, Any] = {}
+
+        for key, value in self._default_params.items():
+
+            # If the value is a list, we check each item in this list
+            # for a function called 'as_dict', meaning they are subclasses of
+            # WallhavenModel. If the item is not a subclass of WallhavenModel,
+            # we append the value directly.
+            # E.g. Wallpapers have a list of Tags and a list of Colors.
+            # Each tag is a subclass of WallhavenModel, whereas each color is
+            # just a string.
+            if isinstance(getattr(self, key, None), list):
+                data[key] = []
+                for subobj in getattr(self, key, None):
+                    if getattr(subobj, "as_dict", None):
+                        data[key].append(subobj.as_dict())
+                    else:
+                        data[key].append(subobj)
+
+            # Not a list, but still a subclass of WallhavenModel and we can
+            # assign the data[key] directly with key.as_dict().
+            # An example being an Uploader object.
+            elif getattr(getattr(self, key, None), "as_dict", None):
+                data[key] = getattr(self, key).as_dict()
+
+            # If the value doesn't have an `as_dict` method, meaning is not a subclass
+            # of WallhavenModel, we can asign the value directly.
+            else:
+                data[key] = getattr(self, key, None)
+
+        return data
+
+    def as_json(self, **kwargs) -> str:
+        """Return the instance as a JSON string.
+
+        Args:
+            kwargs: Additional parameters that ``json.dumps`` takes.
+
+        Returns:
+            A valid JSON string.
+        """
+        return json.dumps(self.as_dict(), **kwargs)
+
+    @classmethod
+    def new_from_dict(cls, data: Dict[str, Any]) -> "WallhavenModel":
+        """Return a new instance based on a JSON dict.
+
+        Parameters:
+            data (Dict[str, Any]): A JSON dict.
+        """
+        # Create new instance
+        c = cls(**data)
+
+        # Save original data in a hidden variable just in case.
+        c._json = data
+
+        return c
+
+
+class Wallpaper(WallhavenModel):
+    """Model class that represents wallhaven's wallpaper."""
+
+    def __init__(self, **kwargs):
+        self._default_params = {
+            "id": None,
+            "purity": None,
+            "url": None,
+            "short_url": None,
+            "uploader": None,
+            "views": None,
+            "favorites": None,
+            "source": None,
+            "purity": None,
+            "category": None,
+            "dimension_x": None,
+            "dimension_y": None,
+            "resolution": None,
+            "ratio": None,
+            "file_size": None,
+            "file_type": None,
+            "created_at": None,
+            "colors": None,
+            "path": None,
+            "thumbs": None,
+            "tags": None,
+        }
+
+        for param, default in self._default_params.items():
+            setattr(self, param, kwargs.get(param, default))
+
+        if "uploader" in kwargs:
+            self.uploader = Uploader.new_from_dict(kwargs["uploader"])
+
+        if "tags" in kwargs:
+            self.tags = [Tag.new_from_dict(tag) for tag in kwargs["tags"]]
+
+    def __str__(self):
+        return (
+            "Wallpaper(id={0.id}, url={0.url}, file_type={0.file_type}, "
+            "purity={0.purity}, resolution={0.resolution})".format(self)
+        )
+
+
+class Uploader(WallhavenModel):
+    """Model class that represents an uploader."""
+
+    def __init__(self, **kwargs):
+        self._default_params = {"username": None, "group": None, "avatar": None}
+
+        for param, default in self._default_params.items():
+            setattr(self, param, kwargs.get(param, default))
+
+
+class Tag(WallhavenModel):
+    """Model class that represents a wallpapers' tags."""
+
+    def __init__(self, **kwargs):
+        self._default_params = {
+            "id": None,
+            "name": None,
+            "alias": None,
+            "category_id": None,
+            "category": None,
+            "purity": None,
+            "created_at": None,
+        }
+
+        for param, default in self._default_params.items():
+            setattr(self, param, kwargs.get(param, default))
+
+
+class SearchResults(WallhavenModel):
+    """A class that represents the API search results."""
+
+    def __init__(self, **kwargs):
+        self._default_params = {"data": None, "meta": None}
+
+        for param, default in self._default_params.items():
+            setattr(self, param, kwargs.get(param, default))
+
+        if "data" in kwargs:
+            self.data = [Wallpaper.new_from_dict(wall) for wall in kwargs["data"]]
+
+        if "meta" in kwargs:
+            self.meta = Meta.new_from_dict(kwargs["meta"])
+
+    def __str__(self):
+        return "SearchResults(data={data}, meta={meta})".format(
+            data=[str(wall) for wall in self.data], meta=self.meta
+        )
+
+
+class Meta(WallhavenModel):
+    """Model class that represents the API search results' meta information."""
+
+    def __init__(self, **kwargs):
+        self._default_params = {
+            "current_page": None,
+            "last_page": None,
+            "per_page": None,
+            "total": None,
+            "query": None,
+            "seed": None,
+            "url": None,
+        }
+
+        for param, default in self._default_params.items():
+            setattr(self, param, kwargs.get(param, default))
+
+
+class Settings(WallhavenModel):
+    """Model class that represents an user's settings."""
+
+    def __init__(self, **kwargs):
+        self._default_params = {
+            "thumb_size": None,
+            "per_page": None,
+            "purity": None,
+            "categories": None,
+            "resolutions": None,
+            "aspect_ratios": None,
+            "toplist_range": None,
+            "tag_blacklist": None,
+            "user_blacklist": None,
+        }
+
+        for param, default in self._default_params.items():
+            setattr(self, param, kwargs.get(param, default))
+
+
+class Collection(WallhavenModel):
+    def __init__(self, **kwargs):
+        self._default_params = {
+            "id": None,
+            "label": None,
+            "views": None,
+            "public": None,
+            "count": None,
+        }
+
+        for param, default in self._default_params.items():
+            setattr(self, param, kwargs.get(param, default))
+
+
+class CollectionData(WallhavenModel):
+    def __init__(self, **kwargs):
+        self._default_params = {"data": None, "meta": None}
+
+        for param, default in self._default_params.items():
+            setattr(self, param, kwargs.get(param, default))
+
+        if "data" in kwargs:
+            self.data = [Wallpaper.new_from_dict(w) for w in kwargs["data"]]
+
+        if "meta" in kwargs:
+            self.meta = Meta.new_from_dict(kwargs["meta"])
--- wallhaven/utils.py
+++ wallhaven/utils.py
@@ -1,3 +1,5 @@
+from typing import Dict, Any
+
 import requests
 
 from wallhaven.exceptions import WallhavenException
@@ -32,3 +34,18 @@ def get_url_for_route(route: str) -> str:
         raise WallhavenException("Invalid route: " + route)
 
     return url
+
+
+def dict_to_string(data: Dict[str, Any]) -> str:
+    """Convert a dictionary into a string. The string is formatted as key=value."""
+    result = ""
+
+    for key, value in data.items():
+        # Add quotes if it's a string.
+        if isinstance(value, str):
+            result += "{key}='{value}', ".format(key=key, value=value)
+        else:
+            result += "{key}={value}, ".format(key=key, value=value)
+
+    # Return the result without the last space and comma.
+    return result[:-2]
